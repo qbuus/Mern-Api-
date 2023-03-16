851baec25fa7a85d4cf9eb08be5b688d18d0c6f3
qbuus <jkocz867@gmail.com>
2023-03-16 13:04:32 +0100

    auth helper

---
+++ src/helpers/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import crypto from "crypto";
+
+const SECRET = process.env.SECRET as string;
+
+export const random = () => {
+  crypto.randomBytes(128).toString("base64");
+};
+
+export const authentication = (
+  salt: string,
+  password: string
+) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
