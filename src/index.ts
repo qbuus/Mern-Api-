5105094f614eecac49fb10edb9e7488c35a486d2
qbuus <jkocz867@gmail.com>
2023-03-16 11:45:48 +0100

    htpp server creation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("d");
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+
+const PORT = process.env.SERVER_PORT || 8001;
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
